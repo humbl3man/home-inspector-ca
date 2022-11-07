95571987e0ebe12c578199231031299b62cea18e
Konstantin Minevich <humbl3dev@gmail.com>
2022-11-06 22:10:05 -0800

    mobile nav improvements

--- app/components/DesktopNav.tsx
+++ app/components/DesktopNav.tsx
@@ -5,15 +5,16 @@ type DesktopNavProps = {
   isIndexRoute?: boolean;
 };
 export function DesktopNav({ navLinks, isIndexRoute }: DesktopNavProps) {
-  const navLinkClass = `text-base ${isIndexRoute ? 'text-white' : 'text-lime-800'}`;
-  const activeNavLinkClass = 'underline';
-
   return (
     <nav className={`hidden sm:block`}>
       <ul className="grid gap-4 mt-0 ml-0 grid-flow-col ">
         {navLinks.map((link) => {
           return (
-            <NavLink prefetch="intent" key={link.id} className={({ isActive }) => (isActive ? `${activeNavLinkClass} ${navLinkClass}` : navLinkClass)} to={link.href}>
+            <NavLink
+              prefetch="intent"
+              key={link.id}
+              className={({ isActive }) => `font-semibold hover:opacity-80 ${isIndexRoute ? 'text-white' : 'text-black'} ${isActive ? 'opacity-100' : 'opacity-70'}`}
+              to={link.href}>
               {link.label}
             </NavLink>
           );
