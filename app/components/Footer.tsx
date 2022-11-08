1994920602743bbe41503269503e28c9dc1099d2
Konstantin M <humbl3dev@gmail.com>
2022-11-08 12:53:02 -0800

    update footer styling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,8 @@
 export function Footer() {
   return (
-    <footer className="bg-gray-100">
+    <footer className="bg-orange-100">
       <div className="mx-auto max-w-7xl py-4 px-2">
-        <div className="text-center text-xs">
+        <div className="text-center text-xs text-orange-900">
           &copy; {new Date().getFullYear()} Home Inspectors California. All
           Rights Reserved.
         </div>
