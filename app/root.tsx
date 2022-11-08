ddc0539269e5c6de46892d00771309e551387f30
Konstantin M <humbl3dev@gmail.com>
2022-11-08 12:52:47 -0800

    add custom 404 page

--- app/root.tsx
+++ app/root.tsx
@@ -11,25 +11,29 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
   useLocation
 } from '@remix-run/react';
 import { Footer } from './components/Footer';
 import Navbar from './components/Navbar';
 
 import favIcon from '../public/favicon.png';
+import notFoundImage from '../public/construction.svg';
 
 import styles from './tailwind.css';
 
+const SITE_TITLE = 'Home Inspectors California';
+
 export const loader: LoaderFunction = () => {
   return json({
-    siteTitle: 'Home Inspectors California'
+    siteTitle: SITE_TITLE
   });
 };
 
-export const meta: MetaFunction = ({ data }) => {
+export const meta: MetaFunction = () => {
   return {
     charset: 'utf-8',
-    title: data.siteTitle,
+    title: SITE_TITLE,
     viewport: 'width=device-width,initial-scale=1'
   };
 };
@@ -45,6 +49,51 @@ export const links: LinksFunction = () => [
   }
 ];
 
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="min-h-screen">
+        <div className="grid h-screen grid-rows-[auto_minmax(auto,_1fr)_auto]">
+          <div>
+            <Navbar isIndexRoute={false} />
+          </div>
+          <main className="mt-8 mb-16">
+            {caught.status === 404 ? (
+              <div className="mx-auto max-w-xl px-2">
+                <img
+                  className="mx-auto my-12 block w-96 object-fill"
+                  src={notFoundImage}
+                  alt="Page Not Found"
+                />
+                <h1 className="mb-4 text-center text-2xl font-bold">
+                  404 Page Not Found
+                </h1>
+                <p className="text-center opacity-80">
+                  We are sorry, but the page you are looking for is not found.
+                </p>
+              </div>
+            ) : (
+              <div>We are sorry, but we encountered an error.</div>
+            )}
+          </main>
+          <div>
+            <Footer />
+          </div>
+        </div>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
 export default function App() {
   const location = useLocation();
   const isIndexRoute = location.pathname === '/';
@@ -55,7 +104,13 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <>
+        <div
+          className={
+            isIndexRoute
+              ? ''
+              : 'grid h-screen grid-rows-[auto_minmax(auto,_1fr)_auto]'
+          }
+        >
           <Navbar isIndexRoute={isIndexRoute} />
           {isIndexRoute ? (
             <Outlet />
@@ -65,7 +120,7 @@ export default function App() {
             </main>
           )}
           {!isIndexRoute ? <Footer /> : null}
-        </>
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
