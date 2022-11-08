4895877d9a7678d5cc870785290df64ce21d47bf
Konstantin M <humbl3dev@gmail.com>
2022-11-08 09:27:36 -0800

    homepage animations

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,7 @@
 import type { MetaFunction, LinksFunction } from '@remix-run/node';
 import { Link } from '@remix-run/react';
+import { motion } from 'framer-motion';
+
 import homeBackgroundImage from '../../public/home-background-image.jpg';
 
 export const meta: MetaFunction = ({ parentsData }) => {
@@ -28,19 +30,44 @@ export default function IndexRoute() {
           alt=""
         />
         <div className="relative z-10 mx-auto max-w-7xl px-2">
-          <h1 className="mb-8 text-5xl font-bold uppercase leading-none tracking-tight text-white sm:text-6xl md:text-7xl">
-            Home Inspectors <br /> California
-          </h1>
-          <p className="mb-8 text-2xl text-lime-300">
-            Bringing Peace Of Mind Back To Your Home
-          </p>
-          <Link
-            prefetch="intent"
-            to="contact"
-            className="rounded-md bg-lime-500 px-4 py-3 text-lg text-black transition-colors duration-300 hover:bg-lime-600"
+          <motion.h1
+            initial={{
+              opacity: 0,
+              y: -50
+            }}
+            animate={{
+              opacity: 100,
+              y: 0
+            }}
+            className="mb-8 text-5xl font-bold uppercase leading-none tracking-tight text-white sm:text-6xl md:text-7xl"
           >
-            Request a Quote
-          </Link>
+            Home Inspectors <br /> California
+          </motion.h1>
+          <motion.div
+            initial={{
+              opacity: 0,
+              y: 50
+            }}
+            animate={{
+              opacity: 100,
+              y: 0
+            }}
+            transition={{
+              delay: 0.3
+            }}
+          >
+            <p className="mb-8 text-2xl text-lime-300">
+              Bringing Peace Of Mind Back To Your Home.
+            </p>
+            <Link
+              prefetch="intent"
+              to="contact"
+              className="rounded-md bg-lime-500 px-4 py-3 text-lg text-black transition-colors duration-300 hover:bg-lime-600"
+            >
+              Request a Quote
+            </Link>
+            s
+          </motion.div>
         </div>
         <div className="absolute inset-0 h-full w-full bg-[rgba(196,99,72,0.71)] mix-blend-multiply" />
       </div>
